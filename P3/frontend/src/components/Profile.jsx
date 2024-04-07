3ac4f4da71264797a3a4053aa3df9608a88dc59e
Aaron <aa.tran@mail.utoronto.ca>
2024-04-07 01:27:44 -0400

    updated profile

--- P3/frontend/src/components/Profile.jsx
+++ P3/frontend/src/components/Profile.jsx
@@ -23,7 +23,7 @@ const Profile = () => {
 
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDay()).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
   };
 
@@ -34,7 +34,13 @@ const Profile = () => {
     } else {
       setTimeError('');
     }
+
+    if (successMessage){
+      setSuccessMessage("");
+    }
   };
+
+  console.log(day);
 
   const handleEndTimeChange = (event) => {
     setEndTime(event.target.value);
@@ -42,6 +48,9 @@ const Profile = () => {
       setTimeError('End time cannot be before start time.');
     } else {
       setTimeError('');
+    }
+    if (successMessage){
+      setSuccessMessage("");
     }
   };
 
--- P3/startup.sh
+++ P3/startup.sh
@@ -2,6 +2,7 @@ python -m venv venv
 source venv/bin/activate
 pip install djangorestframework
 pip install djangorestframework-simplejwt
-cd OneOnOne
+pip install django-cors-headers
+cd backend
 python manage.py makemigrations
 python manage.py migrate
